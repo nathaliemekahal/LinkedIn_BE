14aad39881bebec2674be236a4d447b75c3a9396
Nathalie <nathaliemekahal@gmail.com>
2020-07-20 14:22:46 +0200

    CRUD Completed

--- services/Routes/Experience/index.js
+++ services/Routes/Experience/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const q2m = require("query-to-mongo");
 const ExperienceModel = require("./schema");
+const e = require("express");
 
 const experienceRoute = express.Router();
 
@@ -10,6 +11,39 @@ experienceRoute.post("/", async (req, res) => {
   await newExperience.save();
 
   res.status(201).send("ok");
+});
+
+experienceRoute.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const experience = await ExperienceModel.findById(id);
+    res.send(experience);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+experienceRoute.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const experience = await ExperienceModel.findByIdAndUpdate(
+    id,
+    {
+      ...req.body,
+    },
+    { runValidators: true }
+  );
+  if (experience) {
+    res.send("UPDATED");
+  }
+});
+
+experienceRoute.delete("/:id", async (req, res) => {
+  try {
+    await ExperienceModel.findByIdAndDelete(req.params.id);
+    res.send("deleted");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = experienceRoute;
